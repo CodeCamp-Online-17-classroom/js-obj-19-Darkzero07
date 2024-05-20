84571c51f6326c97d19f9451844cf2595d0d69aa
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-20 23:04:10 +0700

    js lab 65

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+var name = 'Joe';
+function makeUser() {
+  return {
+    name: 'John',
+    ref: this
+  };
+}
+let user = makeUser();
+console.log(user.ref.name); // undefined 
+// no reference object for this in makeuser()
+
